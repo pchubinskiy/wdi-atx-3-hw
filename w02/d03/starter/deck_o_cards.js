8f9501055ad67e5307e71f170bf849ba26447576
Matt Duncan <busman84@gmail.com>
2015-12-09 17:08:08 -0600

    Adding HW w02d03

---
+++ w02/d03/starter/deck_o_cards.js
@@ -0,0 +1,18 @@
+// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays. 
+// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+function deck_o_cards() {
+	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  	var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+	// Create the arrays for the deck, and shuffled deck
+	var cards = [];
+	// Make 52 card objects and store them in the "cards" array
+
+	// Shuffle the Deck
+
+	// Pull the top card
+
+	// Console log the results
+}
+
+// Helpful Tips: Research Fisher Yates Shuffle Algorithm
