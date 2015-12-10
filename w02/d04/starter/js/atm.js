ec56924331b31579bfe799d7b11473d4c3e2d21b
Matt Duncan <busman84@gmail.com>
2015-12-10 15:00:28 -0600

    Adding HW w02d04

---
+++ w02/d04/starter/js/atm.js
@@ -0,0 +1,50 @@
+//Begin with the document ready function
+
+	//Begin work on the Checking Account
+
+		//Checking account deposit function
+
+			//Get value from the amountChecking input field
+
+			//On click of the depositChecking button
+
+				//Take that value and add it to the existing value in the checkingBalance div
+
+		//Checking account withdrawl funtion 
+
+			//Get value from the amountChecking input field
+			
+			//On click of the withdrawChecking button
+
+				// If that value is greater than the value in the account ignore that action
+				// In other words if this would put the account into a negative balance do not allow it
+
+				//Else subtract that value from the current amount in the checking account
+
+	//Begin work on the Savings account
+
+		//Savings account deposit function
+
+		 		//Get value from the amountSavings input field
+
+		 		//On click of the depositSavings button
+
+		 			//Take that value and add it to the existing value in the savingsBalance div
+
+		//Savings account withdraw funtion 
+
+		 		//Get value from the amountSavings input field
+				
+		 		//On click of the withdrawl button
+
+		 			//If that value is greater than the value in the account ignore that action
+		 			//In other words if this would put the account into a negative balance do not allow it
+
+		 			//Else subtract that value from the current amount in the savings account
+
+
+
+// Bonus-- get the two accounts to work with each other and allow for overdraft protection
+
+// Double Bonus-- This isnt very DRY.  Using the keyword "this" see if you can make the withdraw and deposit functions work for both accounts
+
