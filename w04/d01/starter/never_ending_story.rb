55daff1be75f49e5eb2032cea9bf21fb07d9f55a
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-01-04 23:39:50 -0600

    got everything but redo/retry to work

--- w04/d01/starter/never_ending_story.rb
+++ w04/d01/starter/never_ending_story.rb
@@ -5,3 +5,33 @@ story = [
   'The spaceship hits a wormhole and crashes in 1941 Roswell, NM',
   'Fry encounters his grandmother, then inadvertently sires his father'
 ]
+
+# story.each do |x|
+
+#   print x + ". Should we continue? (y/n)"
+#   answer = gets.chomp.downcase
+#   if answer == "y"
+#     print x + ". "
+#     if x === 'Fry encounters his grandmother, then inadvertently sires his father'
+#       redo
+#     end
+#   elsif answer == "n"
+#     break
+#   else
+#     redo
+#   end
+# end
+
+
+for x in story
+
+  print x + ". Should we continue? (y/n)"
+  answer = gets.chomp.downcase
+  if answer == "y"
+    print x + ". "
+    retry if x === 'Fry encounters his grandmother, then inadvertently sires his father'
+  elsif answer == "n"
+    break
+  else
+  end
+end
