abd3caccbb7e70508fb21bd904dc71c27651c80d
Matt Duncan <busman84@gmail.com>
2016-01-26 16:58:58 -0600

    adding HW w07d02

---
+++ w07/d02/starter/geom.js
@@ -0,0 +1,28 @@
+function Rectangle(length, width) {
+  this.length = length;
+  this.width = width;
+}
+
+Rectangle.prototype =  {
+	// Your code goes here
+};
+
+function Triangle(sideA, sideB, sideC) {
+  this.sideA = sideA;
+  this.sideB = sideB;
+  this.sideC = sideC;
+}
+Triangle.prototype = {
+	// Your code goes here
+};
+
+
+function LineSegment(x1, y1, x2, y2) {
+  this.x1 = x1;
+  this.y1 = y1;
+  this.x2 = x2;
+  this.y2 = y2;
+}
+LineSegment.prototype = {
+	// Your code goes here
+};
