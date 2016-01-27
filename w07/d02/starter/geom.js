cbb2f801d01b7ca7129efa3533efcef6cb326a39
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-01-27 10:12:01 -0600

    w07d02 hw

--- w07/d02/starter/geom.js
+++ w07/d02/starter/geom.js
@@ -4,7 +4,18 @@ function Rectangle(length, width) {
 }
 
 Rectangle.prototype =  {
-	// Your code goes here
+  isSquare: function() {
+    console.log((2*this.length + 2*this.width));
+    if ((2*this.length + 2*this.width) === (4*this.length)) {
+      return true;
+    }
+  },
+  area: function() {
+    return this.length * this.width;
+  },
+  perimeter: function() {
+    return this.length + this.width + this.length + this.width;
+  }
 };
 
 function Triangle(sideA, sideB, sideC) {
@@ -13,9 +24,25 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 Triangle.prototype = {
-	// Your code goes here
-};
+  isEquilateral: function() {
+    if ((this.sideA === this.sideB) && (this.sideB === this.sideC)) {
+      return true;
+    }
+  },
+  isIscoceles: function() {
+    if ((this.sideA === this.sideB) && (this.sideB !== this.sideC)) {
+      return true;
+    } else if ((this.sideA !== this.sideB) && (this.sideB === this.sideC)) {
+      return true;
+    }
+  },
+  area: function() { //1/2 * h*b
 
+  },
+  isObtuse: function() { //one angle > 90
+
+  }
+};
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
@@ -24,5 +51,24 @@ function LineSegment(x1, y1, x2, y2) {
   this.y2 = y2;
 }
 LineSegment.prototype = {
-	// Your code goes here
+  length: function() { //(x1+x2)**2 + (y1+y2)**2
+    return Math.sqrt( Math.pow(Math.abs(this.x1+this.x2), 2) + Math.pow(Math.abs(this.y1+this.y2), 2) );
+  }
 };
+
+var rect = new Rectangle(4, 6);
+console.log(rect.isSquare());
+console.log(rect.area());
+console.log(rect.perimeter());
+
+var tri = new Triangle(3, 3, 3);
+console.log(tri.isEquilateral());
+console.log(tri.isIscoceles());
+console.log(tri.area());
+console.log(tri.isObtuse());
+
+var line = new LineSegment(1, 1, 4, 4);
+console.log(line.length());
+
+
+
