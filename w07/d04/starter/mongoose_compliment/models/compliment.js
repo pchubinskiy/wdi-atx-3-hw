62d81bb1239d16241a659e69fd8584e39d1958ed
Sean Shannon <seans887@gmail.com>
2016-01-29 17:43:26 -0600

    Adding line to Compliment model to use schema

--- w07/d04/starter/mongoose_compliment/models/compliment.js
+++ w07/d04/starter/mongoose_compliment/models/compliment.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var Compliment = new Schema({
+var complimentSchema = new Schema({
   compliment: { type: String, required: true }
 });
+
+var Compliment = mongoose.model('Compliment', complimentSchema);
 
 // Make this available to our other files
 module.exports = Compliment;
