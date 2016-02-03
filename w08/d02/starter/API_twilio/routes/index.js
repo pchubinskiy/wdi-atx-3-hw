74049617dcf944893d4253e32e9ce97ff6188afb
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-02-03 10:58:51 -0600

    fixed error by preceding recipient number by '1'; still get error on secret use

--- w08/d02/starter/API_twilio/routes/index.js
+++ w08/d02/starter/API_twilio/routes/index.js
@@ -9,17 +9,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   //res.send("message sent");
-  var number_to = req.body.to;
-  var text_message = req.body.message;
+  //var number_to = req.body.to;
+  //var text_message = req.body.message;
 
   //Twilio Credentials
-  var accountSid = secret.twilio.sId;
-  var authToken = secret.twilio.secret;
+  // var accountSid = secret.twilio.sId;
+  // var authToken = secret.twilio.secret;
+  var accountSid = 'AC72f0baa20a673ad1069a25712718eabc';
+  var authToken = 'd9fa9a74f7eee86964aacde139d5c806';
 
   //require the Twilio module and create a REST client
   var client = require('twilio')(accountSid, authToken);
 
-  client.messages.create({to: "+8325288983", from: "+18325547895", body: "testing",
+  client.messages.create({to: "18325288983", from: "+18325547895", body: "chamber of secrets",
   }, function(err, message) {
     if (err) console.log("error");
     console.log(message.sid);
