1628a436465c8848864b078e52e3057722285839
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-02-03 10:59:57 -0600

    adding secrets file

---
+++ w08/d02/starter/API_twilio/secret.js
@@ -0,0 +1,9 @@
+var secret = {
+  //Friendly Name: twilioAPI_key
+  twilio: {
+   sId: "SKb9e5e41f6aa19f03715c13f87de8a9d6",
+   secret: "OFzbxUKw2f3T9ITacU4Mx3SPqJpYHmqj"
+  }
+}
+
+module.exports = secret;
