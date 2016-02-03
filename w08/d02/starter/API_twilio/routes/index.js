9320a5d565cead65d1a2305e0e814ec00cf6f078
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-02-03 10:44:05 -0600

    finished twilio message app; getting error

--- w08/d02/starter/API_twilio/routes/index.js
+++ w08/d02/starter/API_twilio/routes/index.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var secret = require('../secret');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/'), function(req, res, next) {
-	// Your code goes here
-}
+router.post('/', function(req, res, next) {
+  //res.send("message sent");
+  var number_to = req.body.to;
+  var text_message = req.body.message;
+
+  //Twilio Credentials
+  var accountSid = secret.twilio.sId;
+  var authToken = secret.twilio.secret;
+
+  //require the Twilio module and create a REST client
+  var client = require('twilio')(accountSid, authToken);
+
+  client.messages.create({to: "+8325288983", from: "+18325547895", body: "testing",
+  }, function(err, message) {
+    if (err) console.log("error");
+    console.log(message.sid);
+    //res.send("message sent");
+    //res.render('index', { title: 'Express' });
+  });
+  // console.log(number_to, text_message);
+  // res.render('index', { title: 'Express' });
+});
+
+// router.post('/', function(req, res, next) {
+//   console.log("worked");
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
